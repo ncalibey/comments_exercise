d0ecf125f6471ef4bc86867edeb5ac20a69dc093
Naveed Fida <naveedfida01@gmail.com>
2018-10-05 01:45:36 +0500

    add test for ParentComment

--- client/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
-import App from '../components/App';
-
-it('has a header element', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('header').length).toEqual(1);
-});
