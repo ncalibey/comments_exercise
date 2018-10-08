a202f396d6ec933e8f7ee455c7cffccadae3b930
Naveed Fida <naveedfida01@gmail.com>
2018-10-08 23:27:04 +0500

    add data

---
+++ client/src/lib/data.js
@@ -0,0 +1,138 @@
+var data = [
+  {
+    "id": "7c4ad745-4171-4ca8-bca0-54f083aba32d",
+    "author": "Louisa Leuschke",
+    "body": "Odit eligendi quasi deleniti ipsa cumque iusto ullam.",
+    "postedAt": 1532722744454,
+    "replies_count": 4,
+    "replies": [
+      {
+        "id": "249bec9c-fac7-43ec-95a6-3f81c8057d8d",
+        "comment_id": "7c4ad745-4171-4ca8-bca0-54f083aba32d",
+        "author": "Cydney Robel",
+        "body": "Ratione quibusdam sed doloremque expedita fugit similique et aut.",
+        "postedAt": 1532707761582
+      },
+      {
+        "id": "0f2299ee-c2e8-4db6-a9de-e2df8f1ddf79",
+        "comment_id": "7c4ad745-4171-4ca8-bca0-54f083aba32d",
+        "author": "Rahul Fisher",
+        "body": "Et esse possimus temporibus. Repudiandae omnis error ab magnam repudiandae maxime aspernatur consequatur. Possimus qui id non esse illo ad. Temporibus sit blanditiis recusandae sunt rerum eos voluptatem ducimus ullam. Cupiditate asperiores facere magnam explicabo voluptatem dolores et. Numquam ex aspernatur assumenda nihil sequi optio quis.",
+        "postedAt": 1532666918433
+      },
+      {
+        "id": "fba8c87a-792f-4462-8ac9-feaecaaff00d",
+        "comment_id": "7c4ad745-4171-4ca8-bca0-54f083aba32d",
+        "author": "Bernice Kautzer",
+        "body": "Placeat dolore voluptates quia itaque ipsa.",
+        "postedAt": 1532713804264
+      },
+      {
+        "id": "858a9a20-6e85-4ad5-af29-5ed34f4c7dd1",
+        "comment_id": "7c4ad745-4171-4ca8-bca0-54f083aba32d",
+        "author": "Troy Huel",
+        "body": "Voluptas voluptatem reprehenderit delectus et eius et.",
+        "postedAt": 1532716715832
+      }
+    ]
+  },
+  {
+    "id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+    "author": "Francisca Welch",
+    "body": "Nostrum ea non. Provident repudiandae quis eos fugiat ipsum. Odit ducimus rerum neque sint suscipit nostrum fugiat facere. Consequatur nesciunt quo sit eius. Similique atque quaerat quae excepturi distinctio eius voluptas vel.",
+    "postedAt": 1532678100074,
+    "replies_count": 5,
+    "replies": [
+      {
+        "id": "9ef6c0c7-ee5a-4371-9379-c9d4d8e97db0",
+        "comment_id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+        "author": "Ewald Borer",
+        "body": "Autem dolorem dignissimos eos quasi.",
+        "postedAt": 1532709098870
+      },
+      {
+        "id": "22573f80-26fc-46f8-9d56-46901fa8607d",
+        "comment_id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+        "author": "Crystel Casper",
+        "body": "Autem est perferendis impedit. Harum soluta sit molestiae et qui necessitatibus. Quaerat reprehenderit inventore et id pariatur. Adipisci perferendis in autem. Et labore eos natus laudantium.",
+        "postedAt": 1532664612743
+      },
+      {
+        "id": "1ce60eb0-92b9-4836-a451-33d3cfb782ff",
+        "comment_id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+        "author": "Vergie Okuneva",
+        "body": "Autem ipsum non expedita commodi dolorem id accusantium eveniet. Sunt rerum totam omnis quasi doloremque. Accusamus quaerat aut animi vero iure. Id excepturi nulla et qui adipisci exercitationem ut eos. Sed libero deleniti rerum doloremque neque illo. Occaecati illum vero praesentium impedit provident.",
+        "postedAt": 1532731295298
+      },
+      {
+        "id": "a10f4199-6ce1-429c-b111-e01e06496d6f",
+        "comment_id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+        "author": "Kenna Jacobson",
+        "body": "Qui repellendus et omnis.",
+        "postedAt": 1532719473288
+      },
+      {
+        "id": "89349c97-9caa-4c22-9114-325e7e23d04b",
+        "comment_id": "5755575c-5e1e-4a9b-805b-0c59de9e698d",
+        "author": "Imogene Conroy",
+        "body": "Voluptatum unde eligendi.",
+        "postedAt": 1532667937487
+      }
+    ]
+  },
+  {
+    "id": "abdaecc7-31f4-4b24-939c-d581be4b96a7",
+    "author": "Julio Crona",
+    "body": "Corporis aut veritatis consequatur quasi non et atque non dolore.",
+    "postedAt": 1532697778556,
+    "replies_count": 1,
+    "replies": [
+      {
+        "id": "53a434bc-7cfe-4c5f-8611-138967e55248",
+        "comment_id": "abdaecc7-31f4-4b24-939c-d581be4b96a7",
+        "author": "Bennie Casper",
+        "body": "Quam natus atque sed culpa molestiae. Qui ad possimus dolor qui reiciendis. Nobis consequatur ducimus provident aperiam quas nemo. Nihil assumenda asperiores molestiae occaecati dignissimos molestiae perferendis ducimus error. Aut optio sequi doloremque eius non dolore nisi dignissimos cum. Et excepturi ut aut autem necessitatibus laborum doloremque.",
+        "postedAt": 1532681928524
+      }
+    ]
+  },
+  {
+    "id": "236a8384-e4a0-47f0-9da2-21f73699e9a6",
+    "author": "Carlo Anderson",
+    "body": "Iste aut harum.",
+    "postedAt": 1532711283783,
+    "replies_count": 4,
+    "replies": [
+      {
+        "id": "fb13499e-0edc-4cf1-a859-ad99f463b10e",
+        "comment_id": "236a8384-e4a0-47f0-9da2-21f73699e9a6",
+        "author": "Marilie Olson",
+        "body": "Expedita reiciendis facilis culpa nam aut amet asperiores. Facilis et culpa. Fugiat quia occaecati distinctio voluptatem delectus.",
+        "postedAt": 1532709308476
+      },
+      {
+        "id": "e8cb49ad-a607-430a-8b63-b7255930b0f2",
+        "comment_id": "236a8384-e4a0-47f0-9da2-21f73699e9a6",
+        "author": "Ursula Kovacek",
+        "body": "Eius velit cum voluptatem dolore aliquam. Laboriosam reprehenderit voluptatum doloremque. Temporibus placeat et natus. Veritatis quos officia fugit culpa quo laboriosam est. Qui error consequatur omnis non sunt. Laboriosam sed aliquid corrupti quidem nisi.",
+        "postedAt": 1532646377526
+      },
+      {
+        "id": "86ae24a3-4276-4675-b70d-ce4ce4cd7643",
+        "comment_id": "236a8384-e4a0-47f0-9da2-21f73699e9a6",
+        "author": "Edgar Dooley",
+        "body": "Ut dolore qui molestiae est. Asperiores dolorum reprehenderit tempore illum omnis quia nemo. Quo aliquid atque esse tempore perferendis voluptatem voluptatum et rerum. Est consequatur quia esse sit eum.",
+        "postedAt": 1532665893567
+      },
+      {
+        "id": "9dbbc761-b18c-46df-baa8-1229d79ff795",
+        "comment_id": "236a8384-e4a0-47f0-9da2-21f73699e9a6",
+        "author": "Camylle Ruecker",
+        "body": "Ut repellendus repellendus.",
+        "postedAt": 1532705306991
+      }
+    ]
+  }
+];
+
+export default data;
